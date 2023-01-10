8bd71bf3e4c6bbf018a4cecd9865063d0aed5e07
areedy <a.reedy@gmail.com>
2023-01-09 23:01:20 -0500

    rendering seeded blogs on homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,10 +13,14 @@ router.get('/', async (req, res) => {
     });
 
     // need to serialize
-
+    const blogs = blogData.map((blog) => 
+      blog.get({ plain: true })
+      );
     // render on page
-    res.status(200).json(blogData);
-    res.render('homepage');
+
+    res.render('homepage', {
+      blogs,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
