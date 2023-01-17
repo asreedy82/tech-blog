f2f3ee6b97e68c6c2290ad5c30202691ccbfa053
areedy <a.reedy@gmail.com>
2023-01-16 21:39:07 -0500

    got signup feature to create new user

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { async } = require('seed/lib/seed');
 const { Blog, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
@@ -26,12 +27,12 @@ router.get('/', async (req, res) => {
   }
 });
 
-//get 
-router.get('/dashboard/:id', async (res, req) => {
+//get dashboard
+router.get('/dashboard', async (res, req) => {
   try {
 //get user's blogs
     const userData = await Blog.findAll({
-      where: {user_id: req.params.id},
+      where: {user_id: req.session.user_id},
       include: [
         { model: User }, 
         { model: Comment },
@@ -50,12 +51,34 @@ router.get('/dashboard/:id', async (res, req) => {
   }
 })
 
+//get blog
+router.get('/blog/:id', async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk({
+      include: [
+        { model: Comment },
+        { model: User },
+      ],
+    });
+
+    const blog = blogData.map((blog) => 
+    blog.get({ plain: true })
+    );
+
+    res.render('blog', {
+      blog,
+    })
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
-  /* if (req.session.logged_in) {
-    res.redirect('/profile');
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
     return;
-  } */
+  }
 
   res.render('login');
 });
